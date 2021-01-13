e82af33ab5881655dd3cf34f9bd8cc95d80704cd
Shady Khalifa <shekohex@gmail.com>
2021-01-13 20:16:43 +0000

    clean up and small optimizations.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::net::SocketAddr;
-use std::path::PathBuf;
 use std::sync::Arc;
 
 use ethash::types::BlockHeader;
@@ -9,29 +8,21 @@ use hyper::{Body, Error, Request, Response, Server};
 use parking_lot::RwLock;
 
 mod service;
-mod storage;
+use service::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum State {
-    Idle,
-    GeneratingDag,
-    GeneratingProofs,
-}
-
-type ServerState = Arc<RwLock<State>>;
-
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // init log adapter.
-    tracing_subscriber::fmt()
-        .with_env_filter("debug")
-        .pretty()
-        .with_target(true)
-        .init();
-
+    env_logger::init();
     // the server should only be running locally.
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let state = Arc::new(RwLock::new(State::Idle));
+    let port = std::env::var("SERVER_PORT")
+        .unwrap_or_else(|_| String::from("3000"))
+        .parse()
+        .unwrap_or(3000); // fail-safe and silently.
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let state = Arc::new(RwLock::new(State::default()));
 
     let make_service = make_service_fn(move |_| {
         let state = state.clone();
@@ -60,20 +51,41 @@ struct ProofsPayload {
     rlp: String,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum CurrentState {
+    Idle,
+    GeneratingDag,
+    GeneratingProofs,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct State {
+    proof_service: ProofGeneratorService,
+    current_state: CurrentState,
+}
+
+type ServerState = Arc<RwLock<State>>;
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            proof_service: ProofGeneratorService::new(DagGeneratorService::new(0)),
+            current_state: CurrentState::Idle,
+        }
+    }
+}
+
 // where the magic happens.
 async fn generate_proofs(req: Request<Body>, state: ServerState) -> Result<Response<Body>, Error> {
     // get the request body bytes.
     let body = hyper::body::to_bytes(req).await?;
     let s = state.read();
+    log::debug!("Current State: {:?}", s.current_state);
     // if we are currently generating dag for example.
-    if *s != State::Idle {
+    if s.current_state != CurrentState::Idle {
         // return that we are busy (423 Locked).
         return Ok(Response::builder().status(423).body(Body::empty()).unwrap());
     }
-    // done with this guard.
-    // drop it then!
-    drop(s);
-
     // parse the json to `ProofsPayload`
     let payload: ProofsPayload = match serde_json::from_slice(&body) {
         Ok(p) => p,
@@ -96,33 +108,35 @@ async fn generate_proofs(req: Request<Body>, state: ServerState) -> Result<Respo
     // calculate the epoch from the block number.
     let epoch = header.number / EthereumPatch::epoch_length();
 
-    // paths will be created when DAG generation is started.
-    let cache_path = PathBuf::from(format!("data/{}/cache.bin", epoch));
-    let dataset_path = PathBuf::from(format!("data/{}/dataset.bin", epoch));
-    let dag_exist = cache_path.exists() && dataset_path.exists();
-
-    let storage = storage::DagStorage::new(cache_path, dataset_path);
-    let dag_service = service::DagGeneratorService::new(storage);
+    let dag_loaded = s.proof_service.is_loaded();
+    // done with this guard.
+    // drop it then!
+    drop(s);
 
     // if the DAG (cache and dataset) is not there yet.
-    if !dag_exist {
+    if !dag_loaded {
         // get our state.
         let mut s = state.write();
         // set it that we are currently generating the DAG.
         // so the next Http call returns immediately
         // without dublicating the work.
-        *s = State::GeneratingDag;
+        s.current_state = CurrentState::GeneratingDag;
+        drop(s);
 
         // clone the state to sent to the other task.
         let state = state.clone();
         std::thread::spawn(move || {
-            // this starts generating the cache for `epoch`
-            let _ = dag_service.cache(epoch.as_usize());
-            // and then generate the dataset for `epoch`.
-            let _ = dag_service.dataset(epoch.as_usize());
+            let mut dag_service = service::DagGeneratorService::new(epoch.as_usize());
+            // warm up
+            // try to reload the dag from the disk;
+            let _ = dag_service.reload().is_ok();
+            let mut proof_service = ProofGeneratorService::new(dag_service);
+            proof_service.build_merkle_tree();
             let mut s = state.write();
+            s.proof_service = proof_service;
             // now set that we are Idle, waiting for the next http request.
-            *s = State::Idle;
+            s.current_state = CurrentState::Idle;
+            drop(s);
         });
         // again here, we return after started the DAG generation.
         return Ok(Response::builder().status(423).body(Body::empty()).unwrap());
@@ -130,14 +144,13 @@ async fn generate_proofs(req: Request<Body>, state: ServerState) -> Result<Respo
         // nice we have DAG ready.
         let mut s = state.write();
         // set our state that we are generating proofs.
-        *s = State::GeneratingProofs;
-        drop(s);
-        let proof_service = service::ProofGeneratorService::new(dag_service);
-        let proofs = proof_service.proofs(header);
-        let json = serde_json::to_string(&proofs).unwrap();
+        s.current_state = CurrentState::GeneratingProofs;
+        drop(s); // drop here to make a chance for other threads to catch up.
         let mut s = state.write();
+        let proofs = s.proof_service.proofs(header);
+        let json = serde_json::to_string(&proofs).unwrap();
         // set us back as Idle waiting for next request.
-        *s = State::Idle;
+        s.current_state = CurrentState::Idle;
         return Ok(Response::builder()
             .status(200)
             .body(Body::from(json))
--- src/service.rs
+++ src/service.rs
@@ -1,103 +1,148 @@
+use std::io;
+use std::path::PathBuf;
+
 use byteorder::ByteOrder;
+use ethash::mtree::MerkleTree;
 use ethash::types::*;
 use ethash::{EthereumPatch, LightDAG, Patch};
+use log::{debug, info, warn};
 use serde::{Deserialize, Serialize};
-use tracing::{debug, error, warn};
-
-use crate::storage::*;
 
 /// Generates DAG (Cache, Dataset).
-#[derive(Clone)]
-pub struct DagGeneratorService<S: Storage> {
-    storage: S,
+#[derive(Debug, PartialEq, Eq)]
+pub struct DagGeneratorService {
+    dataset: Vec<u8>,
+    cache: Vec<u8>,
+    epoch: usize,
 }
 
-impl<S> Default for DagGeneratorService<S>
-where
-    S: Storage + Default,
-{
+impl Default for DagGeneratorService {
     fn default() -> Self {
-        Self::new(S::default())
+        Self::new(0)
     }
 }
 
-impl<S: Storage> DagGeneratorService<S> {
-    pub fn new(storage: S) -> Self {
-        Self { storage }
-    }
-
-    pub fn cache(&self, epoch: usize) -> impl AsRef<[u8]> {
-        match self.storage.read_cache(epoch) {
-            Ok(cache) => cache,
-            Err(e) => {
-                error!("Error: {}", e);
-                warn!("DAG Cache is not found for epoch: {}", epoch);
-                debug!("Start Generating DAG Cache for epoch: {}", epoch);
-                let dag = LightDAG::<EthereumPatch>::new(epoch.into());
-                let _ = self.storage.write_cache(epoch, dag.cache);
-                warn!("DAG Cache is ready for epoch: {}", epoch);
-                self.storage.read_cache(epoch).unwrap()
-            }
+impl DagGeneratorService {
+    pub fn new(epoch: usize) -> Self {
+        Self {
+            dataset: Vec::new(),
+            cache: Vec::new(),
+            epoch,
         }
     }
 
-    pub fn dataset(&self, epoch: usize) -> impl AsRef<[u8]> {
-        match self.storage.read_dataset(epoch) {
-            Ok(dataset) => dataset,
-            Err(_) => {
-                warn!("DAG Dataset is not found for epoch: {}", epoch);
-                debug!("Start Generating DAG Dataset for epoch: {}", epoch);
-                let cache = self.cache(epoch);
-                let size = ethash::get_full_size(epoch);
-                let mut dataset = vec![0u8; size];
-                ethash::make_dataset(&mut dataset, cache.as_ref());
-                let _ = self.storage.write_dataset(epoch, dataset);
-                warn!("DAG Dataset is ready for epoch: {}", epoch);
-                self.storage.read_dataset(epoch).unwrap()
-            }
+    pub fn reload(&mut self) -> io::Result<()> {
+        debug!("Reloading Dag Storage..");
+        let dataset = std::fs::read(format!("data/{}/dataset.bin", self.epoch))?;
+        let cache = std::fs::read(format!("data/{}/cache.bin", self.epoch))?;
+        self.cache = cache;
+        self.dataset = dataset;
+        Ok(())
+    }
+
+    pub fn unload_dataset(&mut self) {
+        self.dataset = Vec::new();
+    }
+
+    pub fn cache(&mut self) -> &[u8] {
+        if self.cache.is_empty() {
+            warn!("DAG Cache is not found for epoch: {}", self.epoch);
+            debug!("Start Generating DAG Cache for epoch: {}", self.epoch);
+            let dag = LightDAG::<EthereumPatch>::new(self.epoch.into());
+            let cache_path = PathBuf::from(format!("data/{}/cache.bin", self.epoch));
+            std::fs::create_dir_all(cache_path.parent().unwrap())
+                .expect("failed to create cache path");
+            std::fs::write(cache_path, &dag.cache).expect("failed to write cache to disk");
+            info!("DAG Cache is ready for epoch: {}", self.epoch);
+            self.cache = dag.cache;
+            self.cache.as_slice()
+        } else {
+            self.cache.as_slice()
         }
+    }
+
+    pub fn dataset(&mut self) -> &[u8] {
+        if self.dataset.is_empty() {
+            let epoch = self.epoch;
+            let cache = self.cache();
+            warn!("DAG Dataset is not found for epoch: {}", epoch);
+            debug!("Start Generating DAG Dataset for epoch: {}", epoch);
+            let size = ethash::get_full_size(epoch);
+            let mut output = vec![0u8; size];
+            ethash::make_dataset(&mut output, cache);
+            let dataset_path = PathBuf::from(format!("data/{}/dataset.bin", epoch));
+            std::fs::create_dir_all(dataset_path.parent().unwrap())
+                .expect("failed to create dataset path");
+            std::fs::write(dataset_path, &output).expect("failed to write dataset to disk");
+            warn!("DAG Dataset is ready for epoch: {}", self.epoch);
+            self.dataset = output;
+            self.dataset.as_slice()
+        } else {
+            self.dataset.as_slice()
+        }
+    }
+
+    pub const fn epoch(&self) -> usize {
+        self.epoch
     }
 }
 
 /// Generates Proof for a given Block header.
-#[derive(Clone)]
-pub struct ProofGeneratorService<S: Storage> {
-    dag_service: DagGeneratorService<S>,
+#[derive(Debug, PartialEq)]
+pub struct ProofGeneratorService {
+    dag_service: DagGeneratorService,
+    loaded: bool,
+    mt: ethash::mtree::MerkleTree,
 }
 
-impl<S: Storage> ProofGeneratorService<S> {
-    pub fn new(dag_service: DagGeneratorService<S>) -> Self {
-        Self { dag_service }
+impl Eq for ProofGeneratorService {}
+
+impl ProofGeneratorService {
+    pub fn new(dag_service: DagGeneratorService) -> Self {
+        Self {
+            dag_service,
+            loaded: false,
+            mt: MerkleTree::Zero(0),
+        }
     }
 
-    pub fn proofs(&self, header: BlockHeader) -> BlockWithProofs {
-        debug!("Start calculating the proofs for #{}", header.number);
+    pub fn is_loaded(&self) -> bool {
+        self.loaded
+    }
+
+    pub fn build_merkle_tree(&mut self) {
+        let epoch = self.dag_service.epoch();
+        let dataset = self.dag_service.dataset();
+        let depth = ethash::calc_dataset_depth(epoch);
+        debug!("Building up the MerkleTree with depth = {}.", depth);
+        let mt = ethash::calc_dataset_merkle_proofs(epoch, dataset);
+        self.dag_service.unload_dataset();
+        self.mt = mt;
+        self.loaded = true;
+        debug!("MerkleTree is ready for epoch: #{}", epoch);
+    }
+
+    pub fn proofs(&mut self, header: BlockHeader) -> BlockWithProofs {
+        debug!("Start calculating the proofs for block #{}", header.number);
         let hash = ethash::seal_header(&BlockHeaderSeal::from(header.clone()));
         let epoch = (header.number / EthereumPatch::epoch_length()).as_usize();
         let full_size = ethash::get_full_size(epoch);
-        let cache = self.dag_service.cache(epoch);
-        let dataset = self.dag_service.dataset(epoch);
+        let cache = self.dag_service.cache();
         debug!("Calculating indices for #{} ..", header.number);
         let indices = ethash::get_indices(hash, header.nonce, full_size, |i| {
-            let raw_data = ethash::calc_dataset_item(cache.as_ref(), i);
+            let raw_data = ethash::calc_dataset_item(cache, i);
             let mut data = [0u32; 16];
             for (i, b) in data.iter_mut().enumerate() {
                 *b = byteorder::LE::read_u32(&raw_data[(i * 4)..]);
             }
             data
         });
-
+        let merkle_root = self.mt.hash();
         let depth = ethash::calc_dataset_depth(epoch);
-        debug!(
-            "Building up the MerkleTree with depth = {} for #{}",
-            depth, header.number
-        );
-        let tree = ethash::calc_dataset_merkle_proofs(epoch, dataset.as_ref());
-
         let mut output = BlockWithProofs {
             number: header.number.as_u64(),
             proof_length: depth as _,
-            merkle_root: hex::encode(tree.hash().0),
+            merkle_root: hex::encode(merkle_root.0),
             elements: Vec::with_capacity(depth * 4),
             merkle_proofs: Vec::with_capacity(depth * 2),
         };
@@ -106,7 +151,7 @@ impl<S: Storage> ProofGeneratorService<S> {
             indices.len()
         );
         for index in &indices {
-            let (element, _, proofs) = tree.generate_proof(*index as _, depth);
+            let (element, _, proofs) = self.mt.generate_proof(*index as _, depth);
             let els = element.into_h256_array();
             let els = els.iter().map(|v| hex::encode(&v.0));
             output.elements.extend(els);
@@ -133,11 +178,7 @@ mod tests {
 
     #[test]
     fn block_2() {
-        tracing_subscriber::fmt()
-            .with_env_filter("debug")
-            .with_test_writer()
-            .pretty()
-            .init();
+        let _ = env_logger::builder().is_test(true).try_init();
         let dag_service = DagGeneratorService::<MockStorage>::default();
         let proof_service = ProofGeneratorService::new(dag_service);
         let block_2_raw = hex::decode("f90218a088e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d4934794dd2f1e6e498202e86d8f5442af596580a4f03c2ca04943d941637411107494da9ec8bc04359d731bfd08b72b4d0edcbd4cd2ecb341a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008503ff00100002821388808455ba4241a0476574682f76312e302e302d30636463373634372f6c696e75782f676f312e34a02f0790c5aa31ab94195e1f6443d645af5b75c46c04fbf9911711198a0ce8fdda88b853fa261a86aa9e").unwrap();
